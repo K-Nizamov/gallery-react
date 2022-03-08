a08a90de417e54761200fa069010da4c0c4b39d0
K-Nizamov <hitman91@abv.bg>
2022-03-08 17:12:20 +0200

    Uploading app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useState, useEffect } from 'react';
+import Card from './components/Card';
+
+
 
 function App() {
+
+  function num1000(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+  const num100 = () => {
+    return (Math.round(Math.random() * 200))
+  }
+
+
+  const server = 'https://dev.meta365.eu/wp-json/wp/v2/image?_embed'
+
+
+  const [data, setData] = useState([])
+  const [currentObject,setCurrentObject] = useState(null)
+
+  useEffect(() => {
+
+    fetch(server)
+      .then(res => res.json())
+      .then(result => {
+        setData(result)
+        setCurrentObject({id:result[0].id,title:result[0].title.rendered,imageUrl:result[0]._embedded['wp:featuredmedia'][0].source_url})
+      } )
+      .catch(err => console.log(err))
+  }, [])
+
+  console.log(data);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div className='current-img'>
+        {
+           currentObject !== null ?
+        <Card 
+        key={currentObject.id + 1000}
+        title = {currentObject.title}
+        imageUrl = {currentObject.imageUrl}
+        /> : ''
+        }
+      </div>
+      {
+        data.map(el => (
+          <Card
+            key={el.id}
+            id={el.id}
+            title={el.title.rendered}
+            imageUrl={el._embedded['wp:featuredmedia'][0].source_url}
+            x={num1000(10, 500)}
+            y={num1000(10, 1100)}
+            rotation={num100()}
+            setCurrentObject = {setCurrentObject}
+          />
+        ))
+      }
+
     </div>
   );
 }
---
+++ src/components/Card.js
@@ -0,0 +1,22 @@
+
+import bmw from '../images/bmw.png'
+
+
+
+function Card({ setCurrentObject, imageUrl, title, id, x, y, rotation }) {
+
+    const degree = `${rotation}deg`
+
+
+
+    return (
+        <>
+            <div onClick={()=>setCurrentObject({id:id,title:title,imageUrl:imageUrl})} className='img-wrapper' style={{ transform: `rotate(${degree})`, position: 'absolute', top: x, left: y }} >
+                <img className='main-img' src={imageUrl} alt="bmw" />
+                <p>{title}</p>
+            </div>
+        </>
+    );
+}
+
+export default Card;
